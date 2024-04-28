08b03c81446526d3e48751fa7bb4bb997e90554e
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-04-28 23:16:56 +0200

    neovim: more bindings for telescope

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -1,9 +1,15 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>pw', builtin.lsp_workspace_symbols, {})
+        vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
+        vim.keymap.set('n', '<leader>lbd', function()
+            builtin.diagnostics({ bufnr = 0 })
+        end, {})
     end
 }
