a4d2e11bb3cc552a55b1af98ecf160c00d393e08
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-07-29 21:46:25 +0200

    neovim: git files picker (telescope)

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -8,5 +8,6 @@ return {
 		vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>bs", builtin.lsp_document_symbols, {})
 		vim.keymap.set("n", "<leader>gs", builtin.lsp_workspace_symbols, {})
+		vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 	end,
 }
