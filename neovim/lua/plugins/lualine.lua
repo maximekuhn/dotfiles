8e7ca1a0e897bb1a86bfe3ce69c62b5f79ea9709
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-07-29 21:43:57 +0200

    neovim: show relative file path (lualine)

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -1,12 +1,19 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require("lualine").setup{
-            options = {
-                theme = "tokyonight",
-            },
-        }
-    end
-
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "tokyonight",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+		})
+	end,
 }
