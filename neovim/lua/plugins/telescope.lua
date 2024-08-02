df7cfb6f6b76450ef9d3ee88cd08a759632facce
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-08-02 10:19:57 +0200

    neovim: telescope buffer diagnostics + updated some plugins

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -9,5 +9,8 @@ return {
 		vim.keymap.set("n", "<leader>bs", builtin.lsp_document_symbols, {})
 		vim.keymap.set("n", "<leader>gs", builtin.lsp_workspace_symbols, {})
 		vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>bd", function()
+			builtin.diagnostics({ bufnr = 0 })
+		end, {})
 	end,
 }
